d31765853608da8c54d0107eae1f93641562c886
abbyli437 <31529490+abbyli437@users.noreply.github.com>
2021-07-12 16:06:34 -0700

    finished settings view
    all controls are wired and ready to handle backend calls.

--- EatsNearMe/ViewControllers/SettingsViewController.m
+++ EatsNearMe/ViewControllers/SettingsViewController.m
@@ -8,11 +8,12 @@
 #import "SettingsViewController.h"
 #import "TTRangeSlider.h"
 
-@interface SettingsViewController ()
+@interface SettingsViewController () <TTRangeSliderDelegate>
 
 @property (strong, nonatomic) TTRangeSlider *priceSlider;
 @property (strong, nonatomic) IBOutlet UIView *contentView;
 @property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
 @property (weak, nonatomic) IBOutlet UISlider *distanceSlider;
 
 @end
@@ -22,14 +23,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self makePriceSlider];
+    [self setUpPriceSlider];
     [self getPriceText];
+    [self setUpDistanceSlider];
+    [self updateDistance:self];
 }
 
-- (void)makePriceSlider {
+- (void)setUpPriceSlider {
     CGRect priceFrame = CGRectMake(self.distanceSlider.frame.origin.x, self.priceLabel.frame.origin.y + 25, self.distanceSlider.frame.size.width, 50);
     self.priceSlider = [[TTRangeSlider alloc] initWithFrame:priceFrame];
-    //[self.priceSlider addTarget:self action:@selector(sliderAction:) forControlEvents:UIControlEventValueChanged];
     
     self.priceSlider.delegate = self;
     self.priceSlider.minValue = 1;
@@ -39,20 +41,43 @@
     self.priceSlider.hideLabels = true;
     self.priceSlider.enableStep = true;
     self.priceSlider.step = 1;
+    self.priceSlider.tintColor = [UIColor grayColor];
+    self.priceSlider.handleColor = [UIColor whiteColor];
+    self.priceSlider.handleDiameter = 25.0;
+    self.priceSlider.handleBorderWidth = 1.0;
+    self.priceSlider.handleBorderColor = [UIColor grayColor];
     
     [self.contentView addSubview:self.priceSlider];
 }
 
+- (void)setUpDistanceSlider {
+    self.distanceSlider.minimumValue = 0;
+    self.distanceSlider.maximumValue = 25;
+}
+
 - (void)getPriceText {
     NSString *priceString = @"";
-    for (int i = 0; i < self.priceSlider.minValue; i++) {
+    for (int i = 0; i < self.priceSlider.selectedMinimum; i++) {
         priceString = [priceString stringByAppendingString:@"$"];
     }
     priceString = [priceString stringByAppendingString:@" - "];
-    for (int i = 0; i < self.priceSlider.maxValue; i++) {
+    for (int i = 0; i < self.priceSlider.selectedMaximum; i++) {
         priceString = [priceString stringByAppendingString:@"$"];
     }
     self.priceLabel.text = priceString;
+}
+
+-(void)rangeSlider:(TTRangeSlider *)sender didChangeSelectedMinimumValue:(float)selectedMinimum andMaximumValue:(float)selectedMaximum {
+    [self getPriceText];
+    
+    //update info on Parse later
+}
+
+- (IBAction)updateDistance:(id)sender {
+    NSString *distString = [NSString stringWithFormat:@"%.2f", self.distanceSlider.value];
+    self.distanceLabel.text = [distString stringByAppendingString:@" miles"];
+    
+    //store new value on parse later
 }
 
 /*
